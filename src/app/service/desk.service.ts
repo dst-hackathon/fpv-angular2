e7cc0a4afa403beb781f0dba2fd47af93baa06f2
Thawee Thaisongsuwan <th.thawee@gmail.com>
2016-11-26 11:26:59 +0700

    Integrate floor-plan-canvas with floor component and add them to plan-dialog

--- src/app/service/desk.service.ts
+++ src/app/service/desk.service.ts
@@ -12,7 +12,7 @@ export class DeskService {
 
   constructor(private http: Http) { }
 
-  getDesks(floorId: String): Observable<Desk[]> {
+  getDesks(floorId): Observable<Desk[]> {
     let headers = new Headers({ 'Content-Type': 'application/json' }); // ... Set content type to JSON
     let options = new RequestOptions({ headers: headers }); // Create a request option
 
