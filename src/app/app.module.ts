e1aedcf027fdff15e1f209fa7429aeb512c236d0
Teetach <teetach.1@gmail.com>
2016-11-26 19:12:40 +0700

    Add missing comma

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -57,7 +57,7 @@ import { PlanholderComponent } from './planholder/planholder.component';
     LeftnavComponent,
     ChangesetListComponent,
     FloorCreateDialogComponent,
-    ChangesetItemComponent
+    ChangesetItemComponent,
     FloorMarkerComponent,
     PlanholderComponent
 
