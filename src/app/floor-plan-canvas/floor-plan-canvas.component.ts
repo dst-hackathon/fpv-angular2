641102a1dad7c7a2f2b091708b96d474eea79271
mekeerati <me.keerati@gmail.com>
2016-11-30 21:37:50 +0700

    remove unused service

--- src/app/floor-plan-canvas/floor-plan-canvas.component.ts
+++ src/app/floor-plan-canvas/floor-plan-canvas.component.ts
@@ -1,13 +1,9 @@
 import {Component, OnInit, Input} from "@angular/core";
 import {Desk} from "../model/desk";
 import {Floor} from "../model/floor";
-import {DeskAssignment} from "../model/desk-assignment";
 import {DeskService} from "../service/desk.service";
 import {Observable} from "rxjs";
-import {ChangesetItem} from "../model/changeset-item";
 import {Changeset} from "../model/changeset";
-import {DeskAssignmentService} from "../service/desk-assignment.service";
-import {ChangesetItemService} from "../service/changeset-item.service";
 @Component({
   selector: 'floor-plan-canvas',
   templateUrl: './floor-plan-canvas.component.html',
@@ -20,16 +16,10 @@ export class FloorPlanCanvasComponent implements OnInit {
   desks: Observable<Desk[]>;
 
   constructor(
-    private deskService:DeskService,
-    private deskAssigmentService: DeskAssignmentService,
-    private changesetItemService: ChangesetItemService,
-
-  ) { }
+    private deskService:DeskService
+  ){}
 
   ngOnInit() {
     this.desks = this.deskService.desks
-
-    if(this.floor) this.deskAssigmentService.loadAll(this.floor.id)
-    if(this.changeset) this.changesetItemService.loadAll(this.changeset.id)
   }
 }
