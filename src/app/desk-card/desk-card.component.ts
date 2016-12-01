f681e4991855811d2d78ee23b5a5107f717234e5
mekeerati <me.keerati@gmail.com>
2016-12-01 21:25:22 +0700

    #7 display employee image for change set item

--- src/app/desk-card/desk-card.component.ts
+++ src/app/desk-card/desk-card.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 
 import { Employee } from '../model/employee';
+import {ChangesetItem} from "../model/changeset-item";
 
 @Component({
   selector: 'desk-card',
@@ -9,10 +10,17 @@ import { Employee } from '../model/employee';
 })
 export class DeskCardComponent implements OnInit {
   @Input() employee: Employee
+  @Input() changesetItem: ChangesetItem
 
+  emp
   constructor() { }
 
   ngOnInit() {
+    if(this.changesetItem && this.changesetItem.employee){
+      this.emp = Object.assign(new Employee(),this.changesetItem.employee)
+    }else{
+      this.emp = this.employee
+    }
   }
 
 }
