e9eb8e9d9e1cbce8f05a3af5d089d288e745bb04
mekeerati <me.keerati@gmail.com>
2016-12-08 02:57:28 +0700

    remove unused import

--- src/app/plan-holder/plan-holder.component.ts
+++ src/app/plan-holder/plan-holder.component.ts
@@ -1,8 +1,6 @@
-import {Component, OnInit, Input} from "@angular/core";
-import {PlanHolderService} from "../service/planholder.service";
+import {Component, OnInit} from "@angular/core";
 import {ChangesetItem} from "../model/changeset-item";
 import {ChangesetItemService} from "../service/changeset-item.service";
-import {ChangesetService} from "../service/changeset.service";
 import {Observable} from "rxjs";
 
 @Component({
