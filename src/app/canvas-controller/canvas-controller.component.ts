415e92791b4c5ab2028468a8d6cd9c762c23d8b9
mekeerati <me.keerati@gmail.com>
2016-12-09 00:16:31 +0700

    #15 fix multiple open dialog issue cause from auto subscribe

--- src/app/canvas-controller/canvas-controller.component.ts
+++ src/app/canvas-controller/canvas-controller.component.ts
@@ -1,8 +1,8 @@
-import {Component, OnInit, Input} from '@angular/core';
+import {Component, OnInit, Input} from "@angular/core";
 import {Desk} from "../model/desk";
 import {Observable} from "rxjs";
 import {DeskService} from "../service/desk.service";
-import {ModalDismissReasons, NgbModal} from "@ng-bootstrap/ng-bootstrap";
+import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
 import {AssignDialogComponent} from "../assign-dialog/assign-dialog.component";
 import {Changeset} from "../model/changeset";
 
@@ -16,27 +16,31 @@ export class CanvasControllerComponent implements OnInit {
   selectedDesk: Observable<Desk>;
   @Input() changeset: Changeset
 
-  constructor(private deskService: DeskService,private modalService: NgbModal) { }
+  private desk;
+
+  constructor(private deskService: DeskService, private modalService: NgbModal) {
+  }
 
   ngOnInit() {
     this.selectedDesk = this.deskService.selectedDesk
+
+    this.selectedDesk.subscribe(desk=>this.desk = desk)
   }
 
   openAssignDialog() {
-    this.selectedDesk.subscribe(desk=>{
-      if(desk){
-        const modalRef = this.modalService.open(AssignDialogComponent)
-        modalRef.componentInstance.desk = desk;
-        modalRef.componentInstance.changeset = this.changeset;
+    let desk = this.desk
+    if (desk) {
+      const modalRef = this.modalService.open(AssignDialogComponent)
+      modalRef.componentInstance.desk = desk;
+      modalRef.componentInstance.changeset = this.changeset;
 
-        modalRef.result.then((result) => {
-          console.log("Modal result",result)
-          this.deskService.setSelectedDesk(null);
-        }, (reason) => {
-          console.log(`close model: ${reason}`)
-          this.deskService.setSelectedDesk(null);
-        });
-      }
-    })
+      modalRef.result.then((result) => {
+        console.log("Modal result", result)
+        this.deskService.setSelectedDesk(null);
+      }, (reason) => {
+        console.log(`close model: ${reason}`)
+        this.deskService.setSelectedDesk(null);
+      });
+    }
   }
 }
