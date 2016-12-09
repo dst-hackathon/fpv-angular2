e5350563d61b594a866582aaec8ec80c3c02666c
mekeerati <me.keerati@gmail.com>
2016-12-09 19:38:29 +0700

    #24 add changeset page

---
+++ src/app/changeset/changeset.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-changeset',
+  templateUrl: './changeset.component.html',
+  styleUrls: ['./changeset.component.css']
+})
+export class ChangesetComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
