f0f1ff4d8e380f79f4b19cc14a92e04c0165c8b7
mekeerati <me.keerati@gmail.com>
2016-12-12 22:22:59 +0700

    #29 support unassign by drag and drop to placeholder

--- src/app/activity/activity.component.ts
+++ src/app/activity/activity.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from "@angular/core";
 import {ChangesetItem} from "../model/changeset-item";
 import {ChangesetItemService} from "../service/changeset-item.service";
 import {Observable} from "rxjs";
+import {Changeset} from "../model/changeset";
+import {ChangesetService} from "../service/changeset.service";
 
 @Component({
   selector: 'app-activity',
@@ -11,16 +13,40 @@ import {Observable} from "rxjs";
 export class ActivityComponent implements OnInit {
 
   changeSetItems: Observable<ChangesetItem[]>
+  changeset: Changeset
 
-  constructor(private changesetItemService: ChangesetItemService) {
+  constructor(private changesetItemService: ChangesetItemService
+    ,private changesetService:ChangesetService) {
   }
 
   ngOnInit() {
     this.changeSetItems = this.changesetItemService.changesetItems
+    this.changesetService.selectedChangeset.subscribe(cs=>this.changeset = cs)
   }
 
   deleteItem(item){
     this.changesetItemService.remove(item.id)
   }
 
+  public ondrag(event) {
+    event.preventDefault()
+  }
+  public ondrop(event) {
+    let fromDesk = JSON.parse(event.dataTransfer.getData('fromDesk'))
+    let employee = JSON.parse(event.dataTransfer.getData('employee'))
+    let changesetItem = this.getTransferData(event,'changesetItem')
+    let toDesk  = null;
+
+    console.log(`Move desk: from ${fromDesk} to ${toDesk}`)
+
+    this.changesetItemService.move(employee, fromDesk, toDesk, this.changeset,changesetItem)
+  }
+  private getTransferData(event,name) {
+    var data = event.dataTransfer.getData(name);
+    if(!data){
+      return null
+    }
+
+    return JSON.parse(data);
+  }
 }
