ca2ea4850afc63c628633fe7db289d4937a80a33
mekeerati <me.keerati@gmail.com>
2017-01-03 23:21:04 +0700

    autofocus input when open

--- src/app/desk-create-dialog/desk-create-dialog.component.ts
+++ src/app/desk-create-dialog/desk-create-dialog.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,Input,Output,EventEmitter } from '@angular/core';
+import {Component, OnInit, Input, Output, EventEmitter, ViewChildren, AfterViewInit} from '@angular/core';
 
 import { Desk } from '../model/desk';
 import { DeskService } from '../service/desk.service';
@@ -7,14 +7,20 @@ import {NgbActiveModal} from "@ng-bootstrap/ng-bootstrap";
 @Component({
   selector: 'desk-create-dialog',
   templateUrl: './desk-create-dialog.component.html',
-  styleUrls: ['./desk-create-dialog.component.css']
+  styleUrls: ['./desk-create-dialog.component.css'],
 })
-export class DeskCreateDialogComponent implements OnInit {
+export class DeskCreateDialogComponent implements OnInit,AfterViewInit {
   @Input() desk: Desk
+
+  @ViewChildren('input') vc;
 
   constructor( private activeModal: NgbActiveModal,private deskService:DeskService) { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    this.vc.first.nativeElement.focus();
   }
 
   save(){
