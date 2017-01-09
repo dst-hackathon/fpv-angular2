1f083e05748bf6853feadc535e54ea6d0f169ef3
mekeerati <me.keerati@gmail.com>
2017-01-09 21:53:00 +0700

    support search desk by employee information

--- src/app/desk.pipe.ts
+++ src/app/desk.pipe.ts
@@ -1,10 +1,14 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import {Desk} from "./model/desk";
+import {ChangesetItemService} from "./service/changeset-item.service";
+import {DeskAssignmentService} from "./service/desk-assignment.service";
 
 @Pipe({
   name: 'desk', pure :false
 })
 export class DeskPipe implements PipeTransform {
+
+  constructor(private changesetItemService : ChangesetItemService,private deskAssignmentService:DeskAssignmentService){}
 
   transform(desks: Desk[], searchText?: string): any {
     if(!searchText){
@@ -15,8 +19,30 @@ export class DeskPipe implements PipeTransform {
       return []
     }
     return desks.filter(desk =>
-      this.contains(desk.code , searchText)
+      this.contains(desk.code , searchText) ||
+      this.searchEmployeeFromDesk(desk,searchText)
     );
+  }
+
+  private searchEmployeeFromDesk(desk,searchText) :boolean{
+    let ciList = []
+      .concat(this.changesetItemService.findByFromDesk(desk))
+      .concat(this.changesetItemService.findByToDesk(desk))
+      .concat(this.deskAssignmentService.findByDesk(desk))
+
+    for (let ci of ciList) {
+      let found = this.contains(
+        ci.employee.firstname,searchText)
+          || this.contains(ci.employee.lastname,searchText)
+          || this.contains(ci.employee.nickname,searchText)
+          || this.contains(ci.employee.code,searchText)
+          || this.contains(ci.employee.work,searchText)
+      if(found){
+        return true;
+      }
+    }
+
+    return false;
   }
 
   private contains(value, searchText: string): boolean {
